40bb6d71623a50e85568b04945d055a58f8c485f
ocolunga <oscar@ancile.dev>
2025-04-22 22:17:07 -0600

    Add mct formula

---
+++ Formula/mct.rb
@@ -0,0 +1,17 @@
+class Mct < Formula
+  desc "macOS Configuration Tools"
+  homepage "https://github.com/ocolunga/mct"
+  url "https://github.com/ocolunga/mct/archive/v0.1.0.tar.gz"
+  sha256 "f716c4ae692755ca896a38edb9b5a1c21e6857d6bcf83818544c5f9056096fa3"
+  license "MIT"
+
+  depends_on "python@3.8"
+
+  def install
+    system "python3", "-m", "pip", "install", *std_pip_args, "."
+  end
+
+  test do
+    system "#{bin}/mct", "--version"
+  end
+end 
