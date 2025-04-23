95a21c2c761e11b0f2e3a0a7b3a60cf6a969b638
ocolunga <oscar@ancile.dev>
2025-04-22 22:22:07 -0600

    Use virtualenv_install_with_resources for installation

--- Formula/mct.rb
+++ Formula/mct.rb
@@ -7,9 +7,13 @@ class Mct < Formula
 
   depends_on "python@3.12"
 
+  resource "hatchling" do
+    url "https://files.pythonhosted.org/packages/d8/a1/7dd1caa87c0b15c04c6291e25112e5d082cce02ee87f221a8be1d594f857/hatchling-1.21.1.tar.gz"
+    sha256 "bba440453a224e7d4478457fa2e8d8c3633765bafa02975a6b53b9bf917980bc"
+  end
+
   def install
-    system "python3", "-m", "pip", "install", "hatchling"
-    system "python3", "-m", "pip", "install", *std_pip_args, "."
+    virtualenv_install_with_resources
   end
 
   test do
